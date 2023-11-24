89823151e14d08c732c47de8fdbb42e2c8b1411c
benaticgrace@gmail.com <benatic.eproc@gmail.com>
2023-11-24 18:07:36 +0530

    Add User - Ben

--- node/components/controllers/user.controller.js
+++ node/components/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const Users_Test = db.users;
-const aesUtil = require("../../assets/js/aesUtil")
+const aesUtil = require("../../assets/js/aesUtil");
+const sequelize = require("sequelize");
 
 // Retrieve all Users_Test from the database.
 exports.findAll = (req, res) => {
@@ -38,46 +39,65 @@ exports.findOne = async (req, res) => {
 };
 
 // Create and Save a new User
+// exports.create = async (req, res) => {
+//   // // Validate request
+//   // if (!req.body.title) {
+//   //   res.status(400).send({
+//   //     message: "Content can not be empty!"
+//   //   });
+//   //   return;
+//   // }
+
+//   let response = JSON.parse(aesUtil.testDecrypt(req.body.stringValue, req.get('auth_token')));
+//   console.log(response);
+
+//   // Create a User Details
+//   const userDetails = {
+//     // UserId: '322342',
+//     Name: response.firstString,
+//     Mobile: "",
+//     Email: "",
+//     Password: response.secondString,
+//     UserIP: "",
+//     Location: "",
+//     UserType: "",
+//     CreatedDateTime: "",
+//     IsActive: "",
+//     IsNotification: ""
+//   };
+// //  res.json(userDetails);
+//   // Save User Details in the database
+//   await Users_Test.create(userDetails)
+//     .then(data => {
+//       console.log(data);
+//       let userDetailsList = {
+//         id: "0",
+//         stringValue: "Saved Sucessfull"
+//       }
+//       res.json(aesUtil.testEncrypt(JSON.stringify(userDetailsList), global.auth_token));
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while creating the Users."
+//       });
+//     });
+// };
+
 exports.create = async (req, res) => {
-  // // Validate request
-  // if (!req.body.title) {
-  //   res.status(400).send({
-  //     message: "Content can not be empty!"
-  //   });
-  //   return;
-  // }
-
   let response = JSON.parse(aesUtil.testDecrypt(req.body.stringValue, req.get('auth_token')));
-
-  // Create a User Details
-  const userDetails = {
-    UserId: '1',
-    Name: response.firstString,
-    Mobile: "",
-    Email: "",
-    Password: response.secondString,
-    UserIP: "",
-    Location: "",
-    UserType: "",
-    createdAt: "",
-    IsActive: "",
-    IsNotification: ""
-  };
-
-  // Save User Details in the database
-  await Users_Test.create(userDetails)
-    .then(data => {
-      console.log(data);
-      let userDetailsList = {
-        id: "0",
-        stringValue: "Saved Sucessfull"
-      }
-      res.json(aesUtil.testEncrypt(JSON.stringify(userDetailsList), global.auth_token));
-    })
+  await db.sequelize.query("INSERT INTO Users_Test (Name, Mobile, Email) VALUES ('test', '4006', 'Norway')").then(data => {
+    console.log(data);
+    let userDetailsList = {
+      id: "0",
+      stringValue: "Saved Sucessfull"
+    }
+    res.json(aesUtil.testEncrypt(JSON.stringify(userDetailsList), global.auth_token));
+  })
     .catch(err => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the Users."
       });
     });
-};
+}
